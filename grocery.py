02f1f36cf150542416664a79df9fd6a0eda8dec8
MasryMostafa <mostafa100@live.ca>
2017-07-26 23:17:22 -0400

    Grocery Revision

--- grocery.py
+++ grocery.py
@@ -1,14 +1,14 @@
 print "Welcome to the grocery list generator."
 glist = []
-a = 1
+a = raw_input("How many items do you have today?")
+b = 0
 
-while a < 2
-    i = raw_input("Enter new grocery item >>>")
+while b < a:
+    i = raw_input("Enter new grocery item >>> ")
     glist.append(i)
-    print "Alright I've got it. Type 1 + RETURN to continue, or 2 + RETURN to end"
-    a = input(">>>")
+    b += 1
 
 print "OK here's your list of groceries:"
 
 for element in glist:
-    print element
+    print "* %s" % element
